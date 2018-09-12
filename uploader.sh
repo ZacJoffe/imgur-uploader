81a58bd4401b09851be5498b34ee77a0b35b4fee
Zac Joffe <zacharyjoffe@gmail.com>
2018-09-12 18:37:45 -0400

    Added uploader functionality

--- uploader.sh
+++ uploader.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+upload()
+{
+    # Replace id with yours here
+    key=986300c20ed68d2
+    curl -X POST -H "Authorization: Client-ID $key" -F "image=$1" https://api.imgur.com/3/image
+    #curl --request POST --url https://api.imgur.com/3/image --header 'authorization: Client-ID YOUR_CLIENTID_HERE' --header 'content-type: multipart/form-data;' -F "image=@/LOCATION/OF/THE/IMAGE.png"
+}
+
+file="$1"
+upload "@$file"

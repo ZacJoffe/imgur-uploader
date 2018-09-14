46b26b57df4c6eb52d0a77f0bc5c5d468ac0a3b2
Zac Joffe <zacharyjoffe@gmail.com>
2018-09-14 19:58:13 -0400

    Used jq to parse json

--- uploader.sh
+++ uploader.sh
@@ -4,9 +4,10 @@ upload()
 {
     # Replace id with yours here
     key=986300c20ed68d2
-    curl -X POST -H "Authorization: Client-ID $key" -F "image=$1" https://api.imgur.com/3/image
-    #curl --request POST --url https://api.imgur.com/3/image --header 'authorization: Client-ID YOUR_CLIENTID_HERE' --header 'content-type: multipart/form-data;' -F "image=@/LOCATION/OF/THE/IMAGE.png"
+    curl -s -X POST -H "Authorization: Client-ID $key" -F "image=$1" https://api.imgur.com/3/image
 }
 
 file="$1"
-upload "@$file"
+output=$(upload "@$file")
+link=$(echo $output | jq .'data' | jq -r .'link')
+echo $link

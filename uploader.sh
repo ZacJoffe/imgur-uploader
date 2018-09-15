b0a6851ebde9484a6fea0194e45cb2e10c18e4a8
Zac Joffe <zacharyjoffe@gmail.com>
2018-09-14 20:34:15 -0400

    Cleaned up and documented code

--- uploader.sh
+++ uploader.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# upload image to imgur using curl, returns raw json 
 upload()
 {
     # Replace id with yours here
@@ -7,7 +8,10 @@ upload()
     curl -s -X POST -H "Authorization: Client-ID $key" -F "image=$1" https://api.imgur.com/3/image
 }
 
-file="$1"
-output=$(upload "@$file")
-link=$(echo $output | jq .'data' | jq -r .'link')
-echo $link
+# put output of upload function into var, passing directory of image as an arg
+output=$(upload "@$1")
+
+# use jq to parse the output and echo link
+echo $output | jq .'data' | jq -r .'link'
+
+# when script is ran, pipe output to xclip to copy link to clipboard
